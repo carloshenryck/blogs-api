13860ab359548b8155899ddc2a859ef62dfae041
Henryck <ryck302@gmail.com>
2022-10-29 20:08:55 -0300

    requisito 17

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,8 +22,15 @@ const getUserById = async (req, res) => {
   res.status(200).json(user);
 };
 
+const deleteUser = async (req, res) => {
+  const { id } = req.user;
+  await userService.deleteUser(id);
+  res.status(204).json();
+};
+
 module.exports = {
   addUser,
   getAllUsers,
   getUserById,
+  deleteUser,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,5 +10,6 @@ router.use(validateToken);
 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
+router.delete('/me', userController.deleteUser);
 
 module.exports = router;
